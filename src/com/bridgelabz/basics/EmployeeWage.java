6469896d45d5b902138801cbfc0b307dd0e89f5b
manu-saini94 <manu.saini94@gmail.com>
2022-08-11 18:57:27 +0530

    welcome to employee wage

---
+++ src/com/bridgelabz/basics/EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.basics;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Calculator");
+    }
+}
